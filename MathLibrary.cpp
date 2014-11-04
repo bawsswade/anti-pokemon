a72ef3cae6cab5228099e254303e11362ed64359
Wade Gushikuma <yochillax@gmail.com>
2014-11-04 15:43:44 -0800

    starting math lib
    added .h and .ccpp

---
+++ MathLibrary.cpp
@@ -0,0 +1,23 @@
+#include "MathLibrary.h"
+
+std::vector<float> operator+(std::vector<float> &other)
+{
+	
+}
+
+std::vector<float> Normalize(float a_x, float a_y)
+{
+
+}
+
+std::vector<float> Normalize(float a_x, float a_y, float a_z)
+{
+
+}
+
+float Magnitude(float a, float b)
+{
+	float c = sqrt((a*a) + (b*b));
+	return c;
+}
+
--- MathLibrary.h
+++ MathLibrary.h
@@ -1,8 +1,33 @@
-#include <vector>
+#include <math.h>
 
 #ifndef _MATH_LIBRARY_H_
 #define _MATH_LIBRARY_H_
 
+class Vector2D
+{
+public:
+	//OVERLOAD OPERATORS
+	Vector2D operator+(Vector2D &other);
 
+	Vector2D operator-(Vector2D &other);
+
+	Vector2D operator*(Vector2D &other);
+
+	Vector2D operator=(Vector2D &other);
+
+	Vector2D operator/(Vector2D &other);
+
+	//Maths
+	Vector2D Normalize(float a_x, float a_y);
+
+	Vector2D Normalize(float a_x, float a_y, float a_z);
+
+private:
+	float x, y;
+};
+
+
+
+float Magnitude(float a, float b);
 
 #endif
