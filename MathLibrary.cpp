91cd8ea0f80d9da728163f92de84a41cab4ccd93
Wade Gushikuma <yochillax@gmail.com>
2014-11-04 17:08:42 -0800

    added overloading ops
    started normalizing vectors

--- MathLibrary.cpp
+++ MathLibrary.cpp
@@ -1,23 +1,88 @@
 #include "MathLibrary.h"
 
-std::vector<float> operator+(std::vector<float> &other)
+Vector2D Vector2D::operator+(Vector2D &other)
 {
-	
+	Vector2D temp;
+	temp.x = x + other.x;
+	temp.y = y + other.y;
+	return temp;
 }
 
-std::vector<float> Normalize(float a_x, float a_y)
+Vector2D Vector2D::operator-(Vector2D &other)
+{
+	Vector2D temp;
+	temp.x = x - other.x;
+	temp.y = y - other.y;
+	return temp;
+}
+
+Vector2D Vector2D::operator*(Vector2D &other)
+{
+	Vector2D temp;
+	temp.x = x * other.x;
+	temp.y = y * other.y;
+	return temp;
+}
+
+Vector2D Vector2D::operator/(Vector2D &other)
+{
+	Vector2D temp;
+	temp.x = x / other.x;
+	temp.y = y / other.y;
+	return temp;
+}
+
+void Vector2D::operator+=(Vector2D &other)
+{
+	x = x + other.x;
+	y = y + other.y;
+}
+
+void Vector2D::operator-=(Vector2D &other)
+{
+	x = x - other.x;
+	y = y - other.y;
+}
+
+void Vector2D::operator*=(Vector2D &other)
+{
+	x = x * other.x;
+	y = y * other.y;
+}
+
+void Vector2D::operator/=(Vector2D &other)
+{
+	x = x / other.x;
+	y = y / other.y;
+}
+
+void Vector2D::operator=(Vector2D &other)
+{
+	x = other.x;
+	y = other.y;
+}
+
+bool Vector2D::operator==(Vector2D &other)
+{
+	return x == other.x && y == other.y;
+}
+
+Vector2D Normalize(Vector2D v)
+{
+	Vector2D temp;
+	temp.x = v.x / Magnitude(v);
+	temp.y = v.y / Magnitude(v);
+	return temp;
+}
+
+Vector2D Normalize(float a_x, float a_y, float a_z)
 {
 
 }
 
-std::vector<float> Normalize(float a_x, float a_y, float a_z)
+float Magnitude(Vector2D v)
 {
-
-}
-
-float Magnitude(float a, float b)
-{
-	float c = sqrt((a*a) + (b*b));
+	float c = sqrt((v.x * v.x) + (v.y * v.y));
 	return c;
 }
 
--- MathLibrary.h
+++ MathLibrary.h
@@ -13,21 +13,29 @@ public:
 
 	Vector2D operator*(Vector2D &other);
 
-	Vector2D operator=(Vector2D &other);
-
 	Vector2D operator/(Vector2D &other);
 
+	void operator+=(Vector2D &other);
+
+	void operator-=(Vector2D &other);
+
+	void operator*=(Vector2D &other);
+
+	void operator/=(Vector2D &other);
+
+	void operator=(Vector2D &other);
+
+	bool operator==(Vector2D &other);
+
 	//Maths
-	Vector2D Normalize(float a_x, float a_y);
+	Vector2D Normalize(Vector2D v);
 
 	Vector2D Normalize(float a_x, float a_y, float a_z);
-
-private:
+//private:
 	float x, y;
 };
 
-
-
-float Magnitude(float a, float b);
+// aka gets hypotonuese
+float Magnitude(Vector2D v);
 
 #endif
