757e320acb31267cea081b6f7a69fc20b1ffecf7
Wade Gushikuma <yochillax@gmail.com>
2014-11-04 17:32:45 -0800

    added overloads for 3d vector
    started on normalizing for 3d vectors.... PROBLEMS

--- MathLibrary.cpp
+++ MathLibrary.cpp
@@ -1,5 +1,6 @@
 #include "MathLibrary.h"
 
+//*************** 2D Vector *************************
 Vector2D Vector2D::operator+(Vector2D &other)
 {
 	Vector2D temp;
@@ -74,11 +75,98 @@ Vector2D Normalize(Vector2D v)
 	temp.y = v.y / Magnitude(v);
 	return temp;
 }
+//******************************************************
 
-Vector2D Normalize(float a_x, float a_y, float a_z)
+
+
+//****************** 3D Vector**************************
+Vector3D Vector3D::operator+(Vector3D &other)
 {
-
+	Vector3D temp;
+	temp.x = x + other.x;
+	temp.y = y + other.y;
+	temp.z = z + other.z;
+	return temp;
 }
+
+Vector3D Vector3D::operator-(Vector3D &other)
+{
+	Vector3D temp;
+	temp.x = x - other.x;
+	temp.y = y - other.y;
+	temp.z = z - other.z;
+	return temp;
+}
+
+Vector3D Vector3D::operator*(Vector3D &other)
+{
+	Vector3D temp;
+	temp.x = x * other.x;
+	temp.y = y * other.y;
+	temp.z = z * other.z;
+	return temp;
+}
+
+Vector3D Vector3D::operator/(Vector3D &other)
+{
+	Vector3D temp;
+	temp.x = x / other.x;
+	temp.y = y / other.y;
+	temp.z = z / other.z;
+	return temp;
+}
+
+void Vector3D::operator+=(Vector3D &other)
+{
+	x = x + other.x;
+	y = y + other.y;
+	z = z + other.z;
+}
+
+void Vector3D::operator-=(Vector3D &other)
+{
+	x = x - other.x;
+	y = y - other.y;
+	z = z - other.z;
+}
+
+void Vector3D::operator*=(Vector3D &other)
+{
+	x = x * other.x;
+	y = y * other.y;
+	z = z * other.z;
+}
+
+void Vector3D::operator/=(Vector3D &other)
+{
+	x = x / other.x;
+	y = y / other.y;
+	z = z / other.z;
+}
+
+void Vector3D::operator=(Vector3D &other)
+{
+	x = other.x;
+	y = other.y;
+	z = other.z;
+}
+
+bool Vector3D::operator==(Vector3D &other)
+{
+	return x == other.x && y == other.y && z == other.z;
+}
+
+// this might not work
+Vector3D Normalize(Vector3D v)
+{
+	Vector3D temp;
+	Magnitude(v.x, v.y);
+	return temp;
+}
+//********************************************************
+
+
+
 
 float Magnitude(Vector2D v)
 {
@@ -86,3 +174,11 @@ float Magnitude(Vector2D v)
 	return c;
 }
 
+
+
+// the normalizing for 3d might not work, making this might not work
+float Magnitude(float x, float y)
+{
+	float c = sqrt((x * x) + (y * y));
+	return c;
+}
--- MathLibrary.h
+++ MathLibrary.h
@@ -35,7 +35,44 @@ public:
 	float x, y;
 };
 
+
+
+
+
+class Vector3D
+{
+public:
+	//OVERLOAD OPERATORS
+	Vector3D operator+(Vector3D &other);
+
+	Vector3D operator-(Vector3D &other);
+
+	Vector3D operator*(Vector3D &other);
+
+	Vector3D operator/(Vector3D &other);
+
+	void operator+=(Vector3D &other);
+
+	void operator-=(Vector3D &other);
+
+	void operator*=(Vector3D &other);
+
+	void operator/=(Vector3D &other);
+
+	void operator=(Vector3D &other);
+
+	bool operator==(Vector3D &other);
+
+	//Maths
+	Vector3D Normalize(Vector3D v);
+
+	Vector3D Normalize(float a_x, float a_y, float a_z);
+	//private:
+	float x, y, z;
+};
+
 // aka gets hypotonuese
 float Magnitude(Vector2D v);
+float Magnitude(float x, float y);
 
 #endif
